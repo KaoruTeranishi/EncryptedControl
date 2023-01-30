0d943623d7b4b2a7cbb40ac46af917775fdae408
Kaoru Teranishi <teranishi@uec.ac.jp>
2023-01-30 17:39:44 +0900

    Refactor to use the same equation in GSW encryption

--- eclib/regev.py
+++ eclib/regev.py
@@ -17,10 +17,10 @@ def keygen(bit_length, n, sigma):
 
     A = np.array([[get_rand(0, params.q) for _ in range(params.m)] for _ in range(params.n)], dtype=object)
     s = np.array([[get_rand(0, params.q)] for _ in range(params.n)], dtype=object)
-    e = np.array(get_int_gaussian(0, sigma, [params.m, 1]), dtype=object)
+    e = np.array(get_int_gaussian(0, sigma, params.m), dtype=object).reshape(-1, 1)
 
-    pk = np.block([[A], [(s.T @ A + e.T) % params.q]])
-    sk = np.block([-s.T % params.q, 1])
+    pk = np.block([[(s.T @ A + e.T) % params.q], [A]])
+    sk = np.block([1, -s.T])
 
     return params, pk, sk
 
@@ -95,7 +95,7 @@ def elementwise_add(params, c1, c2):
     elif isinstance(c1[0][0][0], int) and isinstance(c2[0][0][0], int) and c1.shape == c2.shape:
         return add(params, c1, c2)
     # matrix + vector
-    elif isinstance(c1[0][0][0][0], int) and isinstance(c2[0][0][0], int) and c1.shape == c2.shape:
+    elif isinstance(c1[0][0][0][0], int) and isinstance(c2[0][0][0], int) and c1.shape[1] == c2.shape[0]:
         c = np.zeros(c1.shape, dtype=object)
         for i in range(len(c)):
             for j in range(len(c[0])):
@@ -108,35 +108,35 @@ def elementwise_add(params, c1, c2):
         print('error: elementwise addtion')
         return None
 
-def mult(params, m, c):
+def int_mult(params, m, c):
     # scalar (plaintext) x scalar (ciphertext)
     if isinstance(m, int) and isinstance(c[0][0], int):
-        return _mult(params, m, c)
+        return _int_mult(params, m, c)
     # scalar (plaintext) x vector (ciphertext)
     elif isinstance(m, int) and isinstance(c[0][0][0], int):
         c_ = np.zeros(c.shape, dtype=object)
         for i in range(c_.shape[0]):
-            c_[i] = _mult(params, m, c[i])
+            c_[i] = _int_mult(params, m, c[i])
         return c_
     # scalar (plaintext) x matrix (ciphertext)
     elif isinstance(m, int) and isinstance(c[0][0][0][0], int):
         c_ = np.zeros(c.shape, dtype=object)
         for i in range(c_.shape[0]):
             for j in range(c_.shape[1]):
-                c_[i][j] = _mult(params, m, c[i][j])
+                c_[i][j] = _int_mult(params, m, c[i][j])
         return c_
     # vector (plaintext) x vector (ciphertext)
     elif isinstance(m[0], int) and isinstance(c[0][0][0], int) and m.shape == c.shape:
         c_ = np.zeros(1, dtype=object)
         for i in range(m.shape[0]):
-            c_ = _add(params, c_, _mult(params, m[i], c[i]))
+            c_ = _add(params, c_, _int_mult(params, m[i], c[i]))
         return c_
     # matrix (plaintext) x vector (ciphertext)
     elif isinstance(m[0][0], int) and isinstance(c[0][0][0], int) and m.shape[1] == c.shape[0]:
         c_ = np.zeros(m.shape[0], dtype=object)
         for i in range(m.shape[0]):
             for j in range(m.shape[1]):
-                c_[i] = _add(params, c_[i], _mult(params, m[i][j], c[j]))
+                c_[i] = _add(params, c_[i], _int_mult(params, m[i][j], c[j]))
         return c_
     # matrix (plaintext) x matrix (ciphertext)
     elif isinstance(m[0][0], int) and isinstance(c[0][0][0][0], int) and m.shape[1] == c.shape[0]:
@@ -144,38 +144,44 @@ def mult(params, m, c):
         for i in range(m.shape[0]):
             for j in range(c.shape[1]):
                 for k in range(m.shape[1]):
-                    c_[i][j] = _add(params, c_[i][j], _mult(params, m[i][k], c[k][j]))
+                    c_[i][j] = _add(params, c_[i][j], _int_mult(params, m[i][k], c[k][j]))
         return c_
     else:
-        print('error: multiplication')
+        print('error: integer multiplication')
         return None
 
-def elementwise_mult(params, m, c):
+def elementwise_int_mult(params, m, c):
     # scalar (plaintext) x scalar (ciphertext)
     if isinstance(m, int) and isinstance(c[0][0], int):
-        return mult(params, m, c)
+        return int_mult(params, m, c)
+    # scalar (plaintext) x vector (ciphertext)
+    elif isinstance(m, int) and isinstance(c[0][0][0], int):
+        return int_mult(params, m, c)
+    # scalar (plaintext) x matrix (ciphertext)
+    elif isinstance(m, int) and isinstance(c[0][0][0][0], int):
+        return int_mult(params, m, c)
     # vector (plaintext) x vector (ciphertext)
     elif isinstance(m[0], int) and isinstance(c[0][0][0], int) and m.shape == c.shape:
         c_ = np.zeros(c.shape, dtype=object)
         for i in range(c_.shape[0]):
-            c_[i] = _mult(params, m[i], c[i])
+            c_[i] = _int_mult(params, m[i], c[i])
         return c_
     # matrix (plaintext) x vector (ciphertext)
     elif isinstance(m[0][0], int) and isinstance(c[0][0][0], int) and m.shape[1] == c.shape[0]:
         c_ = np.zeros(m.shape, dtype=object)
-        for i in range(m.shape[0]):
-            for j in range(m.shape[1]):
-                c_[i][j] = _mult(params, m[i][j], c[j])
+        for i in range(c_.shape[0]):
+            for j in range(c_.shape[1]):
+                c_[i][j] = _int_mult(params, m[i][j], c[j])
         return c_
     # matrix (plaintext) x matrix (ciphertext)
     elif isinstance(m[0][0], int) and isinstance(c[0][0][0][0], int) and m.shape == c.shape:
         c_ = np.zeros(c.shape, dtype=object)
         for i in range(c_.shape[0]):
             for j in range(c_.shape[1]):
-                c_[i][j] = _mult(params, m[i][j], c[i][j])
+                c_[i][j] = _int_mult(params, m[i][j], c[i][j])
         return c_
     else:
-        print('error: elementwise multiplication')
+        print('error: elementwise integer multiplication')
         return None
 
 def encode(params, x, delta):
@@ -194,15 +200,15 @@ def dec(params, sk, c, delta):
 
 def _encrypt(params, pk, m):
     r = np.array([[get_rand(0, 2)] for _ in range(params.m)], dtype=object)
-    return (pk @ r + m * np.block([[np.zeros([params.n, 1], dtype=object)], [1]])) % params.q
+    return (pk @ r + m * np.block([[1], [np.zeros([params.n, 1], dtype=object)]])) % params.q
 
 def _decrypt(params, sk, c):
-    return ((sk @ c) % params.q)[0][0]
+    return (sk @ c)[0][0] % params.q
 
 def _add(params, c1, c2):
     return (c1 + c2) % params.q
 
-def _mult(params, m, c):
+def _int_mult(params, m, c):
     return (m * c) % params.q
 
 def _encode(params, x, delta):
