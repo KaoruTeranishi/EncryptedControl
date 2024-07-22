c74caa035389bc96d18fb043af5067127c1b1cdf
Kaoru Teranishi <teranishi@uec.ac.jp>
2024-07-22 11:07:00 -0500

    Refactoring with flake8, black, and isort

--- eclib/elgamal.py
+++ eclib/elgamal.py
@@ -1,23 +1,27 @@
 #! /usr/bin/env python
 
-from eclib.numutils import *
-from eclib.randutils import *
-from eclib.primeutils import *
-from eclib.modutils import *
 from collections import namedtuple
+from math import ceil, floor
+
 import numpy as np
-from math import floor, ceil
+
+import eclib.modutils as mu
+import eclib.numutils as nu
+import eclib.primeutils as pu
+import eclib.randutils as ru
+
 
 def keygen(bit_length):
-    params = namedtuple('Parameters', ['p', 'q', 'g'])
-    params.q, params.p = get_safe_prime(bit_length)
-    params.g = get_generator(params.q, params.p)
+    params = namedtuple("Parameters", ["p", "q", "g"])
+    params.q, params.p = pu.get_safe_prime(bit_length)
+    params.g = pu.get_generator(params.q, params.p)
 
-    sk = get_rand(1, params.q)
+    sk = ru.get_rand(1, params.q)
 
-    pk = mpow(params.g, sk, params.p)
-    
+    pk = mu.mpow(params.g, sk, params.p)
+
     return params, pk, sk
+
 
 def encrypt(params, pk, m):
     if isinstance(m, np.matrix) or isinstance(m, list):
@@ -40,8 +44,9 @@ def encrypt(params, pk, m):
                 c[i][j] = _encrypt(params, pk, m[i][j])
         return c
     else:
-        print('error: encryption')
+        print("error: encryption")
         return None
+
 
 def decrypt(params, sk, c):
     if isinstance(c, np.matrix) or isinstance(c, list):
@@ -64,8 +69,9 @@ def decrypt(params, sk, c):
                 m[i][j] = _decrypt(params, sk, c[i][j])
         return m
     else:
-        print('error: decryption')
+        print("error: decryption")
         return None
+
 
 def mult(params, c1, c2):
     if isinstance(c1, np.matrix) or isinstance(c1, list):
@@ -90,42 +96,57 @@ def mult(params, c1, c2):
                 c[i][j] = _mult(params, c1, c2[i][j])
         return c
     # vector x vector
-    elif isinstance(c1[0][0], int) and isinstance(c2[0][0], int) and c1.shape == c2.shape:
+    elif (
+        isinstance(c1[0][0], int) and isinstance(c2[0][0], int) and c1.shape == c2.shape
+    ):
         c = np.zeros(c1.shape, dtype=object)
         for i in range(c1.shape[0]):
             c[i] = _mult(params, c1[i], c2[i])
         return c
     # matrix x vector
-    elif isinstance(c1[0][0][0], int) and isinstance(c2[0][0], int) and c1.shape[1] == c2.shape[0]:
+    elif (
+        isinstance(c1[0][0][0], int)
+        and isinstance(c2[0][0], int)
+        and c1.shape[1] == c2.shape[0]
+    ):
         c = np.zeros(c1.shape, dtype=object)
         for i in range(c.shape[0]):
             for j in range(c.shape[1]):
                 c[i][j] = _mult(params, c1[i][j], c2[j])
         return c
     # matrix x matrix
-    elif isinstance(c1[0][0][0], int) and isinstance(c2[0][0][0], int) and c1.shape[1] == c2.shape[0]:
+    elif (
+        isinstance(c1[0][0][0], int)
+        and isinstance(c2[0][0][0], int)
+        and c1.shape[1] == c2.shape[0]
+    ):
         c = np.zeros(c1.shape, dtype=object)
         for i in range(c.shape[0]):
             for j in range(c.shape[1]):
                 c[i][j] = _mult(params, c1[i][j], c2[i][j])
         return c
     else:
-        print('error: multiplication')
+        print("error: multiplication")
         return None
 
-def encode(params, x, delta, mode='nearest'):
+
+def encode(params, x, delta, mode="nearest"):
     f = np.frompyfunc(_encode, 4, 1)
     return f(params, x, delta, mode)
+
 
 def decode(params, m, delta):
     f = np.frompyfunc(_decode, 3, 1)
     return f(params, m, delta)
 
-def enc(params, pk, x, delta, mode='nearest'):
+
+def enc(params, pk, x, delta, mode="nearest"):
     return encrypt(params, pk, encode(params, x, delta, mode))
+
 
 def dec(params, sk, c, delta):
     return decode(params, decrypt(params, sk, c), delta)
+
 
 def dec_add(params, sk, c, delta):
     if isinstance(c, np.matrix) or isinstance(c, list):
@@ -146,95 +167,105 @@ def dec_add(params, sk, c, delta):
         for i in range(x.shape[0]):
             for j in range(1, x.shape[1]):
                 x[i][0] += x[i][j]
-        return x[:,0]
+        return x[:, 0]
     else:
-        print('error: decryption with addition')
+        print("error: decryption with addition")
         return None
 
+
 def _encrypt(params, pk, m):
-    r = get_rand(1, params.q)
-    return np.array([mpow(params.g, r, params.p), (m * mpow(pk, r, params.p)) % params.p], dtype=object)
+    r = ru.get_rand(1, params.q)
+    return np.array(
+        [mu.mpow(params.g, r, params.p), (m * mu.mpow(pk, r, params.p)) % params.p],
+        dtype=object,
+    )
+
 
 def _decrypt(params, sk, c):
-    return (minv(mpow(c[0], sk, params.p), params.p) * c[1]) % params.p
+    return (mu.minv(mu.mpow(c[0], sk, params.p), params.p) * c[1]) % params.p
+
 
 def _mult(params, c1, c2):
-    return np.array([(c1[0] * c2[0]) % params.p, (c1[1] * c2[1]) % params.p], dtype=object)
+    return np.array(
+        [(c1[0] * c2[0]) % params.p, (c1[1] * c2[1]) % params.p], dtype=object
+    )
+
 
 def _encode(params, x, delta, mode):
-    if mode == 'nearest':
+    if mode == "nearest":
         m = floor(x / delta + 0.5)
         first_decimal_place = (x / delta * 10) % 10
 
         if m < 0:
             if m < -params.q:
-                print('error: underflow')
+                print("error: underflow")
                 return None
             else:
                 m += params.p
         elif m > params.q:
-            print('error: overflow')
+            print("error: overflow")
             return None
 
         if x / delta == int(x / delta) or first_decimal_place >= 5:
             for i in range(params.q):
-                if m - i > 0 and is_element(m - i, params.q, params.p):
+                if m - i > 0 and nu.is_element(m - i, params.q, params.p):
                     return m - i
-                elif m + i < params.p and is_element(m + i, params.q, params.p):
+                elif m + i < params.p and nu.is_element(m + i, params.q, params.p):
                     return m + i
         else:
             for i in range(params.q):
-                if m + i < params.p and is_element(m + i, params.q, params.p):
+                if m + i < params.p and nu.is_element(m + i, params.q, params.p):
                     return m + i
-                elif m - i > 0 and is_element(m - i, params.q, params.p):
+                elif m - i > 0 and nu.is_element(m - i, params.q, params.p):
                     return m - i
-            
-            print('error: encoding')
+
+            print("error: encoding")
             return None
-    
-    elif mode == 'lower':
+
+    elif mode == "lower":
         m = ceil(x / delta)
 
         if m <= 0:
             if m < -params.q:
-                print('error: underflow')
+                print("error: underflow")
                 return None
             else:
                 m += params.p
         elif m > params.q:
-            print('error: overflow')
+            print("error: overflow")
             return None
 
         for i in range(m):
-            if is_element(m - i, params.q, params.p):
+            if nu.is_element(m - i, params.q, params.p):
                 return m - i
-        
-        print('error: encoding')
+
+        print("error: encoding")
         return None
-    
-    elif mode == 'upper':
+
+    elif mode == "upper":
         m = ceil(x / delta)
 
         if m < 0:
             if m < -params.q:
-                print('error: underflow')
+                print("error: underflow")
                 return None
             else:
                 m += params.p
         elif m > params.q:
-            print('error: overflow')
+            print("error: overflow")
             return None
 
         for i in range(params.p - m):
-            if is_element(m + i, params.q, params.p):
+            if nu.is_element(m + i, params.q, params.p):
                 return m + i
 
-        print('error: encoding')
+        print("error: encoding")
         return None
 
     else:
-        print('error: encoding')
+        print("error: encoding")
         return None
+
 
 def _decode(params, m, delta):
     return (m - params.p) * delta if m > params.q else m * delta
