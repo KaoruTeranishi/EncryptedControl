3bf424c3289090b61d10ccdaef672a0a44782c8a
Kaoru Teranishi <teranishi@uec.ac.jp>
2024-07-22 20:59:02 -0500

    Summarize commands as a function

--- eclib/figsetup.py
+++ eclib/figsetup.py
@@ -3,28 +3,30 @@
 import matplotlib
 import matplotlib.pyplot as plt
 
-# size
-plt.rcParams["figure.figsize"] = (1.62 * 2, 1 * 2)
-# font
-matplotlib.rcParams["pdf.fonttype"] = 42
-matplotlib.rcParams["ps.fonttype"] = 42
-plt.rcParams["font.family"] = "serif"
-plt.rcParams["font.serif"] = ["Times New Roman"]
-plt.rcParams["mathtext.fontset"] = "cm"
-plt.rcParams["font.size"] = 10
-plt.rcParams["ytick.labelsize"] = 10
-plt.rcParams["ytick.labelsize"] = 10
-# axis
-plt.rcParams["xtick.direction"] = "in"
-plt.rcParams["ytick.direction"] = "in"
-plt.rcParams["axes.formatter.use_mathtext"] = True
-plt.rcParams["axes.linewidth"] = 1.0
-plt.rcParams["xtick.major.width"] = 1.0
-plt.rcParams["ytick.major.width"] = 1.0
-# legend
-plt.rcParams["legend.fancybox"] = False
-plt.rcParams["legend.framealpha"] = 1.0
-plt.rcParams["legend.edgecolor"] = "#000000"
-plt.rcParams["legend.handlelength"] = 1.0
-# grid
-plt.rcParams["axes.grid"] = False
+
+def figure_setup() -> None:
+    # size
+    plt.rcParams["figure.figsize"] = (1.62 * 2, 1 * 2)
+    # font
+    matplotlib.rcParams["pdf.fonttype"] = 42
+    matplotlib.rcParams["ps.fonttype"] = 42
+    plt.rcParams["font.family"] = "serif"
+    plt.rcParams["font.serif"] = ["Times New Roman"]
+    plt.rcParams["mathtext.fontset"] = "cm"
+    plt.rcParams["font.size"] = 10
+    plt.rcParams["ytick.labelsize"] = 10
+    plt.rcParams["ytick.labelsize"] = 10
+    # axis
+    plt.rcParams["xtick.direction"] = "in"
+    plt.rcParams["ytick.direction"] = "in"
+    plt.rcParams["axes.formatter.use_mathtext"] = True
+    plt.rcParams["axes.linewidth"] = 1.0
+    plt.rcParams["xtick.major.width"] = 1.0
+    plt.rcParams["ytick.major.width"] = 1.0
+    # legend
+    plt.rcParams["legend.fancybox"] = False
+    plt.rcParams["legend.framealpha"] = 1.0
+    plt.rcParams["legend.edgecolor"] = "#000000"
+    plt.rcParams["legend.handlelength"] = 1.0
+    # grid
+    plt.rcParams["axes.grid"] = False
