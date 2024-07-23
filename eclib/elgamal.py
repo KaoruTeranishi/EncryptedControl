bcd137c639582eec46c8a1d884aebbf19d0e2f1e
Kaoru Teranishi <teranishi@uec.ac.jp>
2024-07-22 20:36:24 -0500

    Add type annotations and exceptions

--- eclib/elgamal.py
+++ eclib/elgamal.py
@@ -1,271 +1,264 @@
 #! /usr/bin/env python3
 
-from collections import namedtuple
-from math import ceil, floor
+from dataclasses import dataclass
+from math import floor
 
 import numpy as np
+from numpy.typing import ArrayLike, NDArray
 
-import eclib.modutils as mu
 import eclib.numutils as nu
 import eclib.primeutils as pu
 import eclib.randutils as ru
+from eclib import exceptions
 
 
-def keygen(bit_length):
-    params = namedtuple("Parameters", ["p", "q", "g"])
-    params.q, params.p = pu.get_safe_prime(bit_length)
-    params.g = pu.get_generator(params.q, params.p)
+@dataclass(frozen=True, slots=True)
+class PublicParameters:
+    p: int
+    q: int
+    g: int
+
+
+def keygen(bit_length: int) -> tuple[PublicParameters, int, int]:
+    q, p = pu.get_safe_prime(bit_length)
+    g = nu.get_generator(q, p)
+    params = PublicParameters(p, q, g)
 
     sk = ru.get_rand(1, params.q)
 
-    pk = mu.mpow(params.g, sk, params.p)
+    pk = pow(params.g, sk, params.p)
 
     return params, pk, sk
 
 
-def encrypt(params, pk, m):
-    if isinstance(m, np.matrix) or isinstance(m, list):
-        m = np.array(m)
+def encrypt(params: PublicParameters, pk: int, m: ArrayLike) -> NDArray[np.object_]:
+    m = np.asarray(m, dtype=object)
 
-    # scalar
-    if isinstance(m, int):
-        return _encrypt(params, pk, m)
-    # vector
-    elif isinstance(m[0], int):
-        c = np.zeros(m.shape, dtype=object)
-        for i in range(c.shape[0]):
-            c[i] = _encrypt(params, pk, m[i])
-        return c
-    # matrix
-    elif isinstance(m[0][0], int):
-        c = np.zeros(m.shape, dtype=object)
-        for i in range(c.shape[0]):
-            for j in range(c.shape[1]):
-                c[i][j] = _encrypt(params, pk, m[i][j])
-        return c
-    else:
-        print("error: encryption")
-        return None
+    match m.ndim:
+        case 0:
+            return _encrypt(params, pk, m.item())
 
+        case 1:
+            return np.array(
+                [_encrypt(params, pk, m[i]) for i in range(m.shape[0])],
+                dtype=object,
+            )
 
-def decrypt(params, sk, c):
-    if isinstance(c, np.matrix) or isinstance(c, list):
-        c = np.array(c)
+        case 2:
+            return np.array(
+                [
+                    [_encrypt(params, pk, m[i][j]) for j in range(m.shape[1])]
+                    for i in range(m.shape[0])
+                ],
+                dtype=object,
+            )
 
-    # scalar
-    if isinstance(c[0], int):
-        return _decrypt(params, sk, c)
-    # vector
-    elif isinstance(c[0][0], int):
-        m = np.zeros(c.shape, dtype=object)
-        for i in range(m.shape[0]):
-            m[i] = _decrypt(params, sk, c[i])
-        return m
-    # matrix
-    elif isinstance(c[0][0][0], int):
-        m = np.zeros(c.shape, dtype=object)
-        for i in range(m.shape[0]):
-            for j in range(m.shape[1]):
-                m[i][j] = _decrypt(params, sk, c[i][j])
-        return m
-    else:
-        print("error: decryption")
-        return None
+        case _:
+            raise exceptions.EncryptionError
 
 
-def mult(params, c1, c2):
-    if isinstance(c1, np.matrix) or isinstance(c1, list):
-        c1 = np.array(c1)
-    if isinstance(c2, np.matrix) or isinstance(c2, list):
-        c2 = np.array(c2)
+def decrypt(params: PublicParameters, sk: int, c: NDArray[np.object_]) -> ArrayLike:
+    c = np.asarray(c, dtype=object)
 
-    # scalar x scalar
-    if isinstance(c1[0], int) and isinstance(c2[0], int):
-        return _mult(params, c1, c2)
-    # scalar x vector
-    elif isinstance(c1[0], int) and isinstance(c2[0][0], int):
-        c = np.zeros(c2.shape, dtype=object)
-        for i in range(c.shape[0]):
-            c[i] = _mult(params, c1, c2[i])
-        return c
-    # scalar x matrix
-    elif isinstance(c1[0], int) and isinstance(c2[0][0][0], int):
-        c = np.zeros(c2.shape, dtype=object)
-        for i in range(c.shape[0]):
-            for j in range(c.shape[1]):
-                c[i][j] = _mult(params, c1, c2[i][j])
-        return c
-    # vector x vector
-    elif (
-        isinstance(c1[0][0], int) and isinstance(c2[0][0], int) and c1.shape == c2.shape
-    ):
-        c = np.zeros(c1.shape, dtype=object)
-        for i in range(c1.shape[0]):
-            c[i] = _mult(params, c1[i], c2[i])
-        return c
-    # matrix x vector
-    elif (
-        isinstance(c1[0][0][0], int)
-        and isinstance(c2[0][0], int)
-        and c1.shape[1] == c2.shape[0]
-    ):
-        c = np.zeros(c1.shape, dtype=object)
-        for i in range(c.shape[0]):
-            for j in range(c.shape[1]):
-                c[i][j] = _mult(params, c1[i][j], c2[j])
-        return c
-    # matrix x matrix
-    elif (
-        isinstance(c1[0][0][0], int)
-        and isinstance(c2[0][0][0], int)
-        and c1.shape[1] == c2.shape[0]
-    ):
-        c = np.zeros(c1.shape, dtype=object)
-        for i in range(c.shape[0]):
-            for j in range(c.shape[1]):
-                c[i][j] = _mult(params, c1[i][j], c2[i][j])
-        return c
-    else:
-        print("error: multiplication")
-        return None
+    match c.ndim - 1:
+        case 0:
+            return _decrypt(params, sk, c)
+
+        case 1:
+            return np.array(
+                [_decrypt(params, sk, c[i]) for i in range(c.shape[0])],
+                dtype=object,
+            )
+
+        case 2:
+            return np.array(
+                [
+                    [_decrypt(params, sk, c[i][j]) for j in range(c.shape[1])]
+                    for i in range(c.shape[0])
+                ],
+                dtype=object,
+            )
+
+        case _:
+            raise exceptions.EncryptionError
 
 
-def encode(params, x, delta, mode="nearest"):
-    f = np.frompyfunc(_encode, 4, 1)
-    return f(params, x, delta, mode)
+def mult(
+    params: PublicParameters, c1: NDArray[np.object_], c2: NDArray[np.object_]
+) -> NDArray[np.object_]:
+    c1 = np.asarray(c1)
+    c2 = np.asarray(c2)
+
+    match c1.ndim - 1:
+        case 0:
+            match c2.ndim - 1:
+                case 0:
+                    return _mult(params, c1, c2)
+
+                case 1:
+                    return np.array(
+                        [_mult(params, c1, c2[i]) for i in range(c2.shape[0])],
+                        dtype=object,
+                    )
+
+                case 2:
+                    return np.array(
+                        [
+                            [_mult(params, c1, c2[i][j]) for j in range(c2.shape[1])]
+                            for i in range(c2.shape[0])
+                        ],
+                        dtype=object,
+                    )
+
+                case _:
+                    raise exceptions.HomomorphicOperationError
+
+        case 1:
+            if c1.shape == c2.shape:
+                return np.array(
+                    [_mult(params, c1[i], c2[i]) for i in range(c1.shape[0])],
+                    dtype=object,
+                )
+
+            else:
+                raise exceptions.HomomorphicOperationError
+
+        case 2:
+            match c2.ndim - 1:
+                case 1:
+                    if c1.shape[1] == c2.shape[0]:
+                        return np.array(
+                            [
+                                [
+                                    _mult(params, c1[i][j], c2[j])
+                                    for j in range(c1.shape[1])
+                                ]
+                                for i in range(c1.shape[0])
+                            ],
+                            dtype=object,
+                        )
+
+                    else:
+                        raise exceptions.HomomorphicOperationError
+
+                case 2:
+                    if c1.shape == c2.shape:
+                        return np.array(
+                            [
+                                [
+                                    _mult(params, c1[i][j], c2[i][j])
+                                    for j in range(c1.shape[1])
+                                ]
+                                for i in range(c1.shape[0])
+                            ],
+                            dtype=object,
+                        )
+
+                    else:
+                        raise exceptions.HomomorphicOperationError
+
+                case _:
+                    raise exceptions.HomomorphicOperationError
+
+        case _:
+            raise exceptions.HomomorphicOperationError
 
 
-def decode(params, m, delta):
+def encode(params: PublicParameters, x: ArrayLike, delta: float) -> ArrayLike:
+    f = np.frompyfunc(_encode, 3, 1)
+    return f(params, x, delta)
+
+
+def decode(params: PublicParameters, m: ArrayLike, delta: float) -> ArrayLike:
     f = np.frompyfunc(_decode, 3, 1)
     return f(params, m, delta)
 
 
-def enc(params, pk, x, delta, mode="nearest"):
-    return encrypt(params, pk, encode(params, x, delta, mode))
+def enc(
+    params: PublicParameters, pk: int, x: ArrayLike, delta: float
+) -> NDArray[np.object_]:
+    return encrypt(params, pk, encode(params, x, delta))
 
 
-def dec(params, sk, c, delta):
+def dec(
+    params: PublicParameters, sk: int, c: NDArray[np.object_], delta: float
+) -> ArrayLike:
     return decode(params, decrypt(params, sk, c), delta)
 
 
-def dec_add(params, sk, c, delta):
-    if isinstance(c, np.matrix) or isinstance(c, list):
-        c = np.array(c)
+def dec_add(
+    params: PublicParameters, sk: int, c: NDArray[np.object_], delta: float
+) -> ArrayLike:
+    c = np.asarray(c)
 
-    # scalar
-    if isinstance(c[0], int):
-        return dec(params, sk, c, delta)
-    # vector
-    elif isinstance(c[0][0], int):
-        x = dec(params, sk, c, delta)
-        for i in range(1, x.shape[0]):
-            x[0] += x[i]
-        return x[0]
-    # matrix
-    elif isinstance(c[0][0][0], int):
-        x = dec(params, sk, c, delta)
-        for i in range(x.shape[0]):
-            for j in range(1, x.shape[1]):
-                x[i][0] += x[i][j]
-        return x[:, 0]
-    else:
-        print("error: decryption with addition")
-        return None
+    match c.ndim - 1:
+        case 0:
+            return dec(params, sk, c, delta)
+
+        case 1:
+            x = dec(params, sk, c, delta)
+            return np.sum(x, axis=0)
+
+        case 2:
+            x = dec(params, sk, c, delta)
+            return np.sum(x, axis=1)
+
+        case _:
+            raise exceptions.DecryptionError
 
 
-def _encrypt(params, pk, m):
+def _encrypt(params: PublicParameters, pk: int, m: int) -> NDArray[np.object_]:
     r = ru.get_rand(1, params.q)
+
     return np.array(
-        [mu.mpow(params.g, r, params.p), (m * mu.mpow(pk, r, params.p)) % params.p],
+        [pow(params.g, r, params.p), (m * pow(pk, r, params.p)) % params.p],
         dtype=object,
     )
 
 
-def _decrypt(params, sk, c):
-    return (mu.minv(mu.mpow(c[0], sk, params.p), params.p) * c[1]) % params.p
+def _decrypt(params: PublicParameters, sk: int, c: NDArray[np.object_]) -> int:
+    return (pow(c[0], -sk, params.p) * c[1]) % params.p
 
 
-def _mult(params, c1, c2):
+def _mult(
+    params: PublicParameters, c1: NDArray[np.object_], c2: NDArray[np.object_]
+) -> NDArray[np.object_]:
     return np.array(
         [(c1[0] * c2[0]) % params.p, (c1[1] * c2[1]) % params.p], dtype=object
     )
 
 
-def _encode(params, x, delta, mode):
-    if mode == "nearest":
-        m = floor(x / delta + 0.5)
-        first_decimal_place = (x / delta * 10) % 10
+def _encode(params: PublicParameters, x: float, delta: float) -> int:
+    m = floor(x / delta + 0.5)
+    first_decimal_place = (x / delta * 10) % 10
 
-        if m < 0:
-            if m < -params.q:
-                print("error: underflow")
-                return None
-            else:
-                m += params.p
-        elif m > params.q:
-            print("error: overflow")
-            return None
-
-        if x / delta == int(x / delta) or first_decimal_place >= 5:
-            for i in range(params.q):
-                if m - i > 0 and nu.is_element(m - i, params.q, params.p):
-                    return m - i
-                elif m + i < params.p and nu.is_element(m + i, params.q, params.p):
-                    return m + i
+    if m < 0:
+        if m < -params.q:
+            raise exceptions.EncodingError("Underflow")
         else:
-            for i in range(params.q):
-                if m + i < params.p and nu.is_element(m + i, params.q, params.p):
-                    return m + i
-                elif m - i > 0 and nu.is_element(m - i, params.q, params.p):
-                    return m - i
+            m += params.p
+    elif m > params.q:
+        raise exceptions.EncodingError("Overflow")
 
-            print("error: encoding")
-            return None
-
-    elif mode == "lower":
-        m = ceil(x / delta)
-
-        if m <= 0:
-            if m < -params.q:
-                print("error: underflow")
-                return None
-            else:
-                m += params.p
-        elif m > params.q:
-            print("error: overflow")
-            return None
-
-        for i in range(m):
-            if nu.is_element(m - i, params.q, params.p):
+    if x / delta == int(x / delta) or first_decimal_place >= 5:
+        for i in range(params.q):
+            if m - i > 0 and nu.is_element(m - i, params.q, params.p):
+                return m - i
+            elif m + i < params.p and nu.is_element(m + i, params.q, params.p):
+                return m + i
+    else:
+        for i in range(params.q):
+            if m + i < params.p and nu.is_element(m + i, params.q, params.p):
+                return m + i
+            elif m - i > 0 and nu.is_element(m - i, params.q, params.p):
                 return m - i
 
-        print("error: encoding")
-        return None
+    raise exceptions.EncodingError
 
-    elif mode == "upper":
-        m = ceil(x / delta)
 
-        if m < 0:
-            if m < -params.q:
-                print("error: underflow")
-                return None
-            else:
-                m += params.p
-        elif m > params.q:
-            print("error: overflow")
-            return None
-
-        for i in range(params.p - m):
-            if nu.is_element(m + i, params.q, params.p):
-                return m + i
-
-        print("error: encoding")
-        return None
+def _decode(params: PublicParameters, m: int, delta: float) -> float:
+    if m > params.q:
+        return (m - params.p) * delta
 
     else:
-        print("error: encoding")
-        return None
-
-
-def _decode(params, m, delta):
-    return (m - params.p) * delta if m > params.q else m * delta
+        return m * delta
---
+++ eclib/exceptions.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+
+
+class EncodingError(Exception):
+    pass
+
+
+class DecodingError(Exception):
+    pass
+
+
+class EncryptionError(Exception):
+    pass
+
+
+class DecryptionError(Exception):
+    pass
+
+
+class HomomorphicOperationError(Exception):
+    pass
