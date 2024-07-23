44c1c91ad932c9c3057a923b81493c7dbf9b5b36
Kaoru Teranishi <teranishi@uec.ac.jp>
2024-07-22 21:26:30 -0500

    Add type annotations

--- eclib/dyn_elgamal.py
+++ eclib/dyn_elgamal.py
@@ -1,86 +1,109 @@
 #! /usr/bin/env python3
 
-from collections import namedtuple
+from dataclasses import dataclass
 
 import numpy as np
+from numpy.typing import ArrayLike, NDArray
 
-import eclib.modutils as mu
 import eclib.randutils as ru
-from eclib import elgamal
+from eclib import elgamal, exceptions
+from eclib.elgamal import PublicParameters
 
 
-def keygen(bit_length):
+@dataclass(slots=True)
+class Token:
+    s: int
+    h: int
+
+
+def keygen(bit_length: int) -> tuple[PublicParameters, int, int]:
     return elgamal.keygen(bit_length)
 
 
-def encrypt(params, pk, m):
+def encrypt(params: PublicParameters, pk: int, m: int) -> NDArray[np.object_]:
     return elgamal.encrypt(params, pk, m)
 
 
-def decrypt(params, sk, c):
+def decrypt(params: PublicParameters, sk: int, c: NDArray[np.object_]) -> ArrayLike:
     return elgamal.decrypt(params, sk, c)
 
 
-def mult(params, c1, c2):
+def mult(
+    params: PublicParameters, c1: NDArray[np.object_], c2: NDArray[np.object_]
+) -> NDArray[np.object_]:
     return elgamal.mult(params, c1, c2)
 
 
-def encode(params, x, delta, mode="nearest"):
-    return elgamal.encode(params, x, delta, mode)
+def encode(params: PublicParameters, x: ArrayLike, delta: float) -> ArrayLike:
+    return elgamal.encode(params, x, delta)
 
 
-def decode(params, m, delta):
+def decode(params: PublicParameters, m: ArrayLike, delta: float) -> ArrayLike:
     return elgamal.decode(params, m, delta)
 
 
-def enc(params, pk, x, delta, mode="nearest"):
-    return elgamal.enc(params, pk, x, delta, mode)
+def enc(
+    params: PublicParameters, pk: int, x: ArrayLike, delta: float
+) -> NDArray[np.object_]:
+    return elgamal.enc(params, pk, x, delta)
 
 
-def dec(params, sk, c, delta):
+def dec(
+    params: PublicParameters, sk: int, c: NDArray[np.object_], delta: float
+) -> ArrayLike:
     return elgamal.dec(params, sk, c, delta)
 
 
-def dec_add(params, sk, c, delta):
+def dec_add(
+    params: PublicParameters, sk: int, c: NDArray[np.object_], delta: float
+) -> ArrayLike:
     return elgamal.dec_add(params, sk, c, delta)
 
 
-def update_key(params, pk, sk):
-    t = namedtuple("token", ["s", "h"])
-    t.s = ru.get_rand(1, params.q)
-    t.h = pk
-    return (pk * mu.mpow(params.g, t.s, params.p)) % params.p, (sk + t.s) % params.q, t
+def update_key(params: PublicParameters, pk: int, sk: int) -> tuple[int, int, Token]:
+    t = Token(ru.get_rand(1, params.q), pk)
+    return (pk * pow(params.g, t.s, params.p)) % params.p, (sk + t.s) % params.q, t
 
 
-def update_ct(params, c, token):
-    # scalar
-    if isinstance(c[0], int):
-        return _update_ct(params, c, token)
-    # vector
-    elif isinstance(c[0][0], int):
-        c_ = np.zeros(c.shape, dtype=object)
-        for i in range(c_.shape[0]):
-            c_[i] = _update_ct(params, c[i], token)
-        return c_
-    # matrix
-    elif isinstance(c[0][0][0], int):
-        c_ = np.zeros(c.shape, dtype=object)
-        for i in range(c_.shape[0]):
-            for j in range(c_.shape[1]):
-                c_[i][j] = _update_ct(params, c[i][j], token)
-        return c_
-    else:
-        print("error: update ciphertext")
-        return None
+def update_ct(
+    params: PublicParameters, c: NDArray[np.object_], t: Token
+) -> NDArray[np.object_]:
+    c = np.asarray(c, dtype=object)
+
+    match c.ndim - 1:
+        case 0:
+            return _update_ct(params, c, t)
+
+        case 1:
+            return np.array(
+                [_update_ct(params, c[i], t) for i in range(c.shape[0])],
+                dtype=object,
+            )
+
+        case 2:
+            return np.array(
+                [
+                    [_update_ct(params, c[i][j], t) for j in range(c.shape[1])]
+                    for i in range(c.shape[0])
+                ],
+                dtype=object,
+            )
+
+        case _:
+            raise exceptions.CiphertextUpdateError
 
 
-def _update_ct(params, c, t):
+def _update_ct(
+    params: PublicParameters, c: NDArray[np.object_], t: Token
+) -> NDArray[np.object_]:
+    c = np.asarray(c, dtype=object)
     r = ru.get_rand(1, params.q)
-    tmp = (c[0] * mu.mpow(params.g, r, params.p)) % params.p
+    tmp = (c[0] * pow(params.g, r, params.p)) % params.p
+
     return np.array(
         [
             tmp,
-            (mu.mpow(tmp, t.s, params.p) * c[1] * mu.mpow(t.h, r, params.p)) % params.p,
+            (pow(tmp, t.s, params.p) * c[1] * pow(t.h, r, params.p)) % params.p,
         ],
         dtype=object,
     )
