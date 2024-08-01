21a9cde37fe668851a3ce5b0ccd595575c76d5f9
Kaoru Teranishi <teranishi@uec.ac.jp>
2024-08-01 12:23:16 -0500

    Add docstring

--- eclib/elgamal.py
+++ eclib/elgamal.py
@@ -13,11 +13,38 @@ import eclib.randutils as ru
 
 @dataclass(slots=True)
 class PublicParameters:
+    """
+    Represents public parameters of the ElGamal encryption scheme.
+
+    Attributes:
+    p: int
+        Prime number representing the modulus of a cyclic group used as the plaintext
+        space.
+    q: int
+        Prime number representing the order of the cyclic group.
+    g: int
+        Generator of the cyclic group.
+    """
+
     p: int
     q: int
     g: int
 
     def __init__(self, bit_length: Optional[int]):
+        """
+        Initializes a new PublicParameters object.
+
+        Args:
+            bit_length (Optional[int]): The desired bit length for a prime number
+                representing the order of a cyclic group used as the plaintext space.
+
+        Note:
+            If `bit_length` is None, the public parameters will be initialized with
+            values of 0. Otherwise, the public parameters will be initialized with
+            a Sophie Germain prime `q` and a safe prime `p` such that `p = 2q + 1`,
+            and a generator `g` of the cyclic group of order `q` modulo `p`.
+        """
+
         if bit_length is None:
             self.p = self.q = self.g = 0
 
@@ -28,9 +55,34 @@ class PublicParameters:
 
 @dataclass(slots=True)
 class SecretKey:
+    """
+    Represents a secret key of the ElGamal encryption scheme.
+
+    Attributes:
+    s: int
+        Secret key value.
+    """
+
     s: int
 
     def __init__(self, params: Optional[PublicParameters]):
+        """
+        Initializes a new SecretKey object.
+
+        Args:
+            params (Optional[PublicParameters]): Cryptosystem parameters used for
+                computing the secret key.
+
+        Note:
+            If `params` is None, the secret key will be initialized with a value of 0.
+            Otherwise, the secret key will be initialized as a random number in
+            [1, q - 1], where q is a prime number representing the order of a cyclic
+            group used as the plaintext space.
+
+        See Also:
+            PublicParameters
+        """
+
         if params is None:
             self.s = 0
 
@@ -40,9 +92,37 @@ class SecretKey:
 
 @dataclass(slots=True)
 class PublicKey:
+    """
+    Represents a public key of the ElGamal encryption scheme.
+
+    Attributes:
+    h: int
+        Public key value.
+    """
+
     h: int
 
     def __init__(self, params: Optional[PublicParameters], sk: Optional[SecretKey]):
+        """
+        Initializes a new PublicKey object.
+
+        Args:
+            params (Optional[PublicParameters]): Cryptosystem parameters used for
+                computing the public key.
+            sk (Optional[SecretKey]): Secret key used for computing the public key.
+
+        Note:
+            If `params` or `sk` is None, the public key will be initialized with a
+            value of 0. Otherwise, the public key will be computed as h = g^s mod p,
+            where g is a generator of a cyclic group used as the plaintext space, s is
+            a secret key value, and p is a prime number representing the modulus of the
+            cyclic group.
+
+        See Also:
+            PublicParameters
+            SecretKey
+        """
+
         if params is None or sk is None:
             self.h = 0
 
@@ -51,6 +131,23 @@ class PublicKey:
 
 
 def keygen(bit_length: int) -> tuple[PublicParameters, PublicKey, SecretKey]:
+    """
+    Generates public parameters, a public key, and a secret key.
+
+    Args:
+        bit_length (int): Desired bit length of a prime number representing
+            the order of a cyclic group used as the plaintext space.
+
+    Returns:
+        tuple[PublicParameters, PublicKey, SecretKey]: Tuple containing the public
+            parameters, public key, and secret key.
+
+    See Also:
+        PublicParameters
+        PublicKey
+        SecretKey
+    """
+
     params = PublicParameters(bit_length)
 
     sk = SecretKey(params)
@@ -63,6 +160,24 @@ def keygen(bit_length: int) -> tuple[PublicParameters, PublicKey, SecretKey]:
 def encrypt(
     params: PublicParameters, pk: PublicKey, m: ArrayLike
 ) -> NDArray[np.object_]:
+    """
+    Encrypts a scalar, vector, or matrix plaintext `m` using a public key `pk`.
+
+    Args:
+        params (PublicParameters): Cryptosystem parameters.
+        pk (PublicKey): Public key used for encryption.
+        m (ArrayLike): Plaintext to be encrypted.
+
+    Returns:
+        NDArray[np.object_]: Ciphertext of the plaintext.
+
+    Raises:
+        ValueError: If the plaintext is not scalar, vector, or matrix.
+
+    See Also:
+        enc
+    """
+
     m = np.asarray(m, dtype=object)
 
     match m.ndim:
@@ -91,6 +206,24 @@ def encrypt(
 def decrypt(
     params: PublicParameters, sk: SecretKey, c: NDArray[np.object_]
 ) -> ArrayLike:
+    """
+    Decrypts a scalar, vector, or matrix ciphertext `c` using a secret key `sk`.
+
+    Args:
+        params (PublicParameters): Cryptosystem parameters.
+        sk (SecretKey): Secret key used for decryption.
+        c (NDArray[np.object_]): Ciphertext to be decrypted.
+
+    Returns:
+        ArrayLike: Decrypted plaintext.
+
+    Raises:
+        ValueError: If the ciphertext is not scalar, vector, or matrix.
+
+    See Also:
+        dec
+    """
+
     c = np.asarray(c, dtype=object)
 
     match c.ndim - 1:
@@ -119,6 +252,24 @@ def decrypt(
 def mult(
     params: PublicParameters, c1: NDArray[np.object_], c2: NDArray[np.object_]
 ) -> NDArray[np.object_]:
+    """
+    Computes the Hadamard product of two scalar, vector, or matrix ciphertexts `c1` and
+    `c2`.
+
+    Args:
+        params (PublicParameters): Cryptosystem parameters.
+        c1 (NDArray[np.object_]): The first ciphertext to be multiplied.
+        c2 (NDArray[np.object_]): The second ciphertext to be multiplied.
+
+    Returns:
+        NDArray[np.object_]: Result of the Hadamard product of the ciphertexts.
+
+    Raises:
+        ValueError: If the ciphertexts are not following the types: scalar-scalar,
+            scalar-vector, scalar-matrix, vector-vector, matrix-vector, or
+            matrix-matrix.
+    """
+
     c1 = np.asarray(c1, dtype=object)
     c2 = np.asarray(c2, dtype=object)
 
@@ -170,11 +321,41 @@ def mult(
 
 
 def encode(params: PublicParameters, x: ArrayLike, delta: float) -> ArrayLike:
+    """
+    Encodes a scalar, vector, or matrix floating-point data `x` into a plaintext.
+
+    Parameters:
+        params (PublicParameters): Cryptosystem parameters.
+        x (ArrayLike): Floating-point data to be encoded.
+        delta (float): Scaling factor.
+
+    Returns:
+        ArrayLike: Encoded plaintext.
+
+    See Also:
+        decode
+    """
+
     f = np.frompyfunc(_encode, 3, 1)
     return f(params, x, delta)
 
 
 def decode(params: PublicParameters, m: ArrayLike, delta: float) -> ArrayLike:
+    """
+    Decodes a scalar, vector, or matrix plaintext `m` into a floating-point data.
+
+    Args:
+        params (PublicParameters): Cryptosystem parameters.
+        m (ArrayLike): Plaintext to be decoded.
+        delta (float): Scaling factor.
+
+    Returns:
+        ArrayLike: Decoded floating-point data.
+
+    See Also:
+        encode
+    """
+
     f = np.frompyfunc(_decode, 3, 1)
     return f(params, m, delta)
 
@@ -182,18 +363,76 @@ def decode(params: PublicParameters, m: ArrayLike, delta: float) -> ArrayLike:
 def enc(
     params: PublicParameters, pk: PublicKey, x: ArrayLike, delta: float
 ) -> NDArray[np.object_]:
+    """
+    Encrypts and encodes a scalar, vector, or matrix floating-point data `x` using a
+    public key `pk`.
+
+    Args:
+        params (PublicParameters): Cryptosystem parameters.
+        pk (PublicKey): Public key used for encryption.
+        x (ArrayLike): Floating-point data to be encoded and encrypted.
+        delta (float): Scaling factor.
+
+    Returns:
+        NDArray[np.object_]: Ciphertext of the encoded plaintext of the floating-point
+            data.
+
+    See Also:
+        encrypt
+        encode
+    """
+
     return encrypt(params, pk, encode(params, x, delta))
 
 
 def dec(
     params: PublicParameters, sk: SecretKey, c: NDArray[np.object_], delta: float
 ) -> ArrayLike:
+    """
+    Decrypts and decodes a scalar, vector, or matrix ciphertext `c` using a secret key
+    `sk`.
+
+    Args:
+        params (PublicParameters): Cryptosystem parameters.
+        sk (SecretKey): Secret key used for decryption.
+        c (NDArray[np.object_]): Ciphertext to be decrypted and decoded.
+        delta (float): Scaling factor.
+
+    Returns:
+        ArrayLike: Decoded floating-point data of the decrypted plaintext.
+
+    See Also:
+        decrypt
+        decode
+    """
+
     return decode(params, decrypt(params, sk, c), delta)
 
 
 def dec_add(
     params: PublicParameters, sk: SecretKey, c: NDArray[np.object_], delta: float
 ) -> ArrayLike:
+    """
+    Decrypts and computes the sum of row-wise elements of a scalar, vector, or matrix
+    ciphertext `c` using a secret key `sk`.
+
+    Args:
+        params (PublicParameters): Cryptosystem parameters.
+        sk (SecretKey): Secret key used for decryption.
+        c (NDArray[np.object_]): Ciphertext to be decrypted and summed.
+        delta (float): Scaling factor.
+
+    Returns:
+        ArrayLike: Decoded floating-point data of the sum of the row-wise elements of
+            the decrypted plaintext.
+
+    Raises:
+        ValueError: If the ciphertext is not scalar, vector, or matrix.
+
+    See Also:
+        dec
+    """
+
     c = np.asarray(c, dtype=object)
 
     match c.ndim - 1:
@@ -211,6 +450,18 @@ def dec_add(
 
 
 def _encrypt(params: PublicParameters, pk: PublicKey, m: int) -> NDArray[np.object_]:
+    """
+    Encrypts a plaintext `m` using a public key `pk`.
+
+    Args:
+        params (PublicParameters): Cryptosystem parameters.
+        pk (PublicKey): Public key used for encryption.
+        m (int): Plaintext to be encrypted.
+
+    Returns:
+        NDArray[np.object_]: Ciphertext of the plaintext.
+    """
+
     r = ru.get_rand(1, params.q)
 
     return np.array(
@@ -220,18 +471,57 @@ def _encrypt(params: PublicParameters, pk: PublicKey, m: int) -> NDArray[np.obje
 
 
 def _decrypt(params: PublicParameters, sk: SecretKey, c: NDArray[np.object_]) -> int:
+    """
+    Decrypts a ciphertext `c` using a secret key `sk`.
+
+    Args:
+        params (PublicParameters): Cryptosystem parameters.
+        sk (SecretKey): Secret key used for decryption.
+        c (NDArray[np.object_]): Ciphertext to be decrypted.
+
+    Returns:
+        int: Decrypted plaintext.
+    """
+
     return (pow(c[0], -sk.s, params.p) * c[1]) % params.p
 
 
 def _mult(
     params: PublicParameters, c1: NDArray[np.object_], c2: NDArray[np.object_]
 ) -> NDArray[np.object_]:
+    """
+    Computes the Hadamard product of two ciphertexts `c1` and `c2`.
+
+    Args:
+        params (PublicParameters): Cryptosystem parameters.
+        c1 (NDArray[np.object_]): The first ciphertext to be multiplied.
+        c2 (NDArray[np.object_]): The second ciphertext to be multiplied.
+
+    Returns:
+        NDArray[np.object_]: Result of the Hadamard product of the ciphertexts.
+    """
+
     return np.array(
         [(c1[0] * c2[0]) % params.p, (c1[1] * c2[1]) % params.p], dtype=object
     )
 
 
 def _encode(params: PublicParameters, x: float, delta: float) -> int:
+    """
+    Encodes a floating-point number `x` into a plaintext.
+
+    Args:
+        params (PublicParameters): Cryptosystem parameters.
+        x (float): Floating-point number to be encoded.
+        delta (float): Scaling factor.
+
+    Returns:
+        int: Encoded plaintext.
+
+    Raises:
+        ValueError: If the encoded value is out of range (underflow or overflow).
+    """
+
     m = floor(x / delta + 0.5)
     first_decimal_place = (x / delta * 10) % 10
 
@@ -265,6 +555,18 @@ def _encode(params: PublicParameters, x: float, delta: float) -> int:
 
 
 def _decode(params: PublicParameters, m: int, delta: float) -> float:
+    """
+    Decodes a plaintext `m` into a floating-point number.
+
+    Args:
+        params (PublicParameters): Cryptosystem parameters.
+        m (int): Plaintext to be decoded.
+        delta (float): Scaling factor.
+
+    Returns:
+        float: Decoded floating-point number.
+    """
+
     if m > params.q:
         return (m - params.p) * delta
 
@@ -273,6 +575,18 @@ def _decode(params: PublicParameters, m: int, delta: float) -> float:
 
 
 def _is_generator(g: int, q: int, p: int) -> bool:
+    """
+    Checks if an integer `g` is a generator of a cyclic group of order `q` modulo `p`.
+
+    Args:
+        g (int): Integer to be checked.
+        q (int): Prime number representing the order of the cyclic group.
+        p (int): Prime number representing the modulus of the cyclic group.
+
+    Returns:
+        bool: True if `g` is a generator of the cyclic group, False otherwise.
+    """
+
     if g <= 1 or g >= p:
         return False
 
@@ -284,6 +598,18 @@ def _is_generator(g: int, q: int, p: int) -> bool:
 
 
 def _is_element(m: int, q: int, p: int) -> bool:
+    """
+    Check if an integer `m` is an element of the cyclic group of order `q` modulo `p`.
+
+    Args:
+        m (int): Integer to be checked.
+        q (int): Prime number representing the order of the cyclic group.
+        p (int): Prime number representing the modulus of the cyclic group.
+
+    Returns:
+        bool: True if `m` is an element of the cyclic group, False otherwise.
+    """
+
     if m <= 0 or m >= p:
         return False
 
@@ -295,6 +621,17 @@ def _is_element(m: int, q: int, p: int) -> bool:
 
 
 def _get_generator(q: int, p: int) -> int:
+    """
+    Returns a generator for the cyclic group of order `q` modulo `p`.
+
+    Args:
+        q (int): Prime number representing the order of the cyclic group.
+        p (int): Prime number representing the modulus of the cyclic group.
+
+    Returns:
+        int: Generator for the cyclib group.
+    """
+
     g = 2
     while _is_generator(g, q, p) is False:
         g += 1
