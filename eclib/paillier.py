6cefbf1cc8f1f6b2ca94b71f94ea5fb0d0593d76
Kaoru Teranishi <teranishi@uec.ac.jp>
2024-08-01 14:47:18 -0500

    Add docstring

--- eclib/paillier.py
+++ eclib/paillier.py
@@ -1,5 +1,46 @@
 #! /usr/bin/env python3
 
+"""
+paiiller.py
+
+This module implements the Paillier encryption scheme, which is a public-key
+cryptosystem based on the decisional composite residuosity assumption. It provides
+functionalities for generating public parameters, public and secret keys, encryption,
+decryption, and homomorphic operations (addition and integer multiplication). It also
+includes functions for encoding and decoding floating-point data into and from
+plaintexts.
+
+Classes:
+    PublicParameters: Represents public parameters of the Paillier encryption scheme.
+    SecretKey: Represents a secret key of the Paillier encryption scheme.
+    PublicKey: Represents a public key of the Paillier encryption scheme.
+
+Functions:
+    keygen: Generates public parameters, a public key, and a secret key.
+    encrypt: Encrypts a scalar, vector, or matrix plaintext.
+    decrypt: Decrypts a scalar, vector, or matrix ciphertext.
+    add: Computes a ciphertext of the addition of two scalar, vector, or matrix
+        plaintexts.
+    elementwise_add: Computes a ciphertext of the elementwise addition of two scalar,
+        vector, or matrix plaintexts.
+    int_mult: Computes a ciphertext of the product of a scalar, vector, or matrix
+        plaintext and another scalar, vector, or matrix plaintext corresponding to a
+        ciphertext.
+    elementwise_int_mult: Computes a ciphertext of the elementwise product of a scalar,
+        vector, or matrix plaintext and another scalar, vector, or matrix plaintext
+        corresponding to a ciphertext.
+    encode: Encodes a scalar, vector, or matrix floating-point data into a plaintext.
+    decode: Decodes a scalar, vector, or matrix plaintext into a floating-point data.
+    enc: Encodes and encrypts a scalar, vector, or matrix floating-point data.
+    dec: Decrypts and decodes a scalar, vector, or matrix ciphertext.
+
+Dependencies:
+    numpy: Fundamental package for scientific computing with Python.
+    eclib.primeutils: Utility functions for generating prime numbers.
+    eclib.randutils: Utility functions for generating random numbers.
+
+"""
+
 from dataclasses import dataclass
 from math import floor, gcd
 
@@ -12,12 +53,29 @@ import eclib.randutils as ru
 
 @dataclass(slots=True)
 class SecretKey:
+    """
+    Represents a secret key of the Paillier encryption scheme.
+
+    Attributes:
+        p (int): The first prime factor.
+        q (int): The second prime factor.
+        lmd (int): Value of Euler's totient function of n = p * q.
+        mu (int): The multiplicative inverse of lmd modulo n.
+    """
+
     p: int
     q: int
     lmd: int
     mu: int
 
     def __init__(self, bit_length: int):
+        """
+        Initializes a new SecretKey object.
+
+        Args:
+            bit_length (int): Desired bit length of semiprime factors.
+        """
+
         self.p, self.q = pu.get_semiprime_factors(bit_length)
         self.lmd = (self.p - 1) * (self.q - 1)
         self.mu = pow(self.lmd, -1, self.p * self.q)
@@ -25,23 +83,79 @@ class SecretKey:
 
 @dataclass(slots=True)
 class PublicParameters:
+    """
+    Represents public parameters of the Paillier encryption scheme.
+
+    Attributes:
+        n (int): Product of two semiprime factors used as the modulus of plaintext
+            space.
+        n_square (int): The square of n used as the modulus of ciphertext space.
+    """
+
     n: int
     n_square: int
 
     def __init__(self, sk: SecretKey):
+        """
+        Initializes a new PublicParameters object.
+
+        Args:
+            sk (SecretKey): Secret key used for computing the public parameters.
+
+        See Also:
+            SecretKey
+        """
+
         self.n = sk.p * sk.q
         self.n_square = self.n**2
 
 
 @dataclass(slots=True)
 class PublicKey:
+    """
+    Represents a public key of the Paillier encryption scheme.
+
+    Attributes:
+        g (int): Public key value computed as n + 1, where n is the product of
+            two semiprime factors used as the modulus of plaintext space.
+    """
+
     g: int
 
     def __init__(self, params: PublicParameters):
+        """
+        Initializes a new PublicKey object.
+
+        Args:
+            params (PublicParameters): Cryptosystem parameters used for computing the
+                public key.
+
+        See Also:
+            PublicParameters
+        """
+
         self.g = params.n + 1
 
 
 def keygen(bit_length: int) -> tuple[PublicParameters, PublicKey, SecretKey]:
+    """
+    Generates public parameters, a public key, and a secret key.
+
+    Args:
+        bit_length (int): Desired bit length of semiprime factors whose product is used
+            as the modulus of plaintext space, and the square of the product is used as
+            the modulus of ciphertext space.
+
+    Returns:
+        tuple[PublicParameters, PublicKey, SecretKey]: Tuple containing the public
+            parameters, public key, and secret key.
+
+    See Also:
+        PublicParameters
+        PublicKey
+        SecretKey
+    """
+
     sk = SecretKey(bit_length)
 
     params = PublicParameters(sk)
@@ -54,6 +168,24 @@ def keygen(bit_length: int) -> tuple[PublicParameters, PublicKey, SecretKey]:
 def encrypt(
     params: PublicParameters, pk: PublicKey, m: ArrayLike
 ) -> int | NDArray[np.object_]:
+    """
+    Encrypts a scalar, vector, or matrix plaintext `m` using a public key `pk`.
+
+    Args:
+        params (PublicParameters): Cryptosystem parameters.
+        pk (PublicKey): Public key used for encryption.
+        m (ArrayLike): Plaintext to be encrypted.
+
+    Returns:
+        int or NDArray[np.object_]: Ciphertext of the plaintext.
+
+    Raises:
+        ValueError: If the plaintext is not scalar, vector, or matrix.
+
+    See Also:
+        enc
+    """
+
     m = np.asarray(m, dtype=object)
 
     match m.ndim:
@@ -82,6 +214,24 @@ def encrypt(
 def decrypt(
     params: PublicParameters, sk: SecretKey, c: int | NDArray[np.object_]
 ) -> ArrayLike:
+    """
+    Decrypts a scalar, vector, or matrix ciphertext `c` using a secret key `sk`.
+
+    Args:
+        params (PublicParameters): Cryptosystem parameters.
+        sk (SecretKey): Secret key used for decryption.
+        c (int or NDArray[np.object_]): Ciphertext to be decrypted.
+
+    Returns:
+        ArrayLike: Decrypted plaintext.
+
+    Raises:
+        ValueError: If the ciphertext is not scalar, vector, or matrix.
+
+    See Also:
+        dec
+    """
+
     c = np.asarray(c, dtype=object)
 
     match c.ndim:
@@ -112,6 +262,26 @@ def add(
     c1: int | NDArray[np.object_],
     c2: int | NDArray[np.object_],
 ) -> int | NDArray[np.object_]:
+    """
+    Computes a ciphertext of the addition of two scalar, vector, or matrix plaintexts
+    corresponding to ciphertexts `c1` and `c2`.
+
+    Args:
+        params (PublicParameters): The public parameters of the Paillier cryptosystem.
+        c1 (int or NDArray[np.object_]): Ciphertext of the first plaintext.
+        c2 (int or NDArray[np.object_]): Ciphertext of the second plaintext.
+
+    Returns:
+        int or NDArray[np.object_]: Ciphertext of the addition of the plaintexts.
+
+    Raises:
+        ValueError: If the ciphertexts are not the following types of appropriate
+            sizes: scalar-scalar, vector-vector, or matrix-matrix.
+
+    See Also:
+        elementwise_add
+    """
+
     c1 = np.asarray(c1, dtype=object)
     c2 = np.asarray(c2, dtype=object)
 
@@ -147,6 +317,27 @@ def elementwise_add(
     c1: int | NDArray[np.object_],
     c2: int | NDArray[np.object_],
 ) -> int | NDArray[np.object_]:
+    """
+    Computes a ciphertext of the elementwise addition of two scalar, vector, or matrix
+    plaintexts corresponding to ciphertexts `c1` and `c2`.
+
+    Args:
+        params (PublicParameters): Cryptosystem parameters.
+        c1 (int or NDArray[np.object_]): Ciphertext of the first plaintext.
+        c2 (int or NDArray[np.object_]): Ciphertext of the second plaintext.
+
+    Returns:
+        int or NDArray[np.object_]: Ciphertext of the elementwise addition of the
+            plaintexts.
+
+    Raises:
+        ValueError: If the ciphertexts are not the following types of appropriate
+            sizes: scalar-scalar, vector-vector, matrix-vector, or matrix-matrix.
+
+    See Also:
+        add
+    """
+
     c1 = np.asarray(c1, dtype=object)
     c2 = np.asarray(c2, dtype=object)
 
@@ -169,6 +360,27 @@ def elementwise_add(
 def int_mult(
     params: PublicParameters, m: ArrayLike, c: int | NDArray[np.object_]
 ) -> int | NDArray[np.object_]:
+    """
+    Computes a ciphertext of the product of a scalar, vector, or matrix plaintext `m`
+    and another scalar, vector, or matrix plaintext corresponding to a ciphertext `c`.
+
+    Args:
+        params (PublicParameters): Cryptosystem parameters.
+        m (ArrayLike): Plaintext to be multiplied.
+        c (int or NDArray[np.object_]): Ciphertext of a plaintext to be multiplied.
+
+    Returns:
+        int or NDArray[np.object_]: Ciphertext of the product of the plaintexts.
+
+    Raises:
+        ValueError: If the plaintext and ciphertext are not the following types of
+            appropriate sizes: scalar-scalar, scalar-vector, scalar-matrix,
+            vector-vector, matrix-vector, or matrix-matrix.
+
+    See Also:
+        elementwise_int_mult
+    """
+
     m = np.asarray(m, dtype=object)
     c = np.asarray(c, dtype=object)
 
@@ -227,6 +439,29 @@ def int_mult(
 def elementwise_int_mult(
     params: PublicParameters, m: ArrayLike, c: int | NDArray[np.object_]
 ) -> int | NDArray[np.object_]:
+    """
+    Computes a ciphertext of the elementwise product of a scalar, vector, or matrix
+    plaintext `m` and another scalar, vector, or matrix plaintext corresponding to a
+    ciphertext `c`.
+
+    Args:
+        params (PublicParameters): Cryptosystem parameters.
+        m (ArrayLike): Plaintext to be multiplied.
+        c (int or NDArray[np.object_]): Ciphertext of a plaintext to be multiplied.
+
+    Returns:
+        int or NDArray[np.object_]: Ciphertext of the elementwise product of the
+            plaintexts.
+
+    Raises:
+        ValueError: If the plaintext and ciphertext are not the following types of
+            appropriate sizes: scalar-scalar, scalar-vector, scalar-matrix,
+            vector-vector, matrix-vector, or matrix-matrix.
+
+    See Also:
+        int_mult
+    """
+
     m = np.asarray(m, dtype=object)
     c = np.asarray(c, dtype=object)
 
@@ -263,11 +498,41 @@ def elementwise_int_mult(
 
 
 def encode(params: PublicParameters, x: ArrayLike, delta: float) -> ArrayLike:
+    """
+    Encodes a scalar, vector, or matrix floating-point data `x` into a plaintext.
+
+    Parameters:
+        params (PublicParameters): Cryptosystem parameters.
+        x (ArrayLike): Floating-point data to be encoded.
+        delta (float): Scaling factor.
+
+    Returns:
+        ArrayLike: Encoded plaintext.
+
+    See Also:
+        decode
+    """
+
     f = np.frompyfunc(_encode, 3, 1)
     return f(params, x, delta)
 
 
 def decode(params: PublicParameters, m: ArrayLike, delta: float) -> ArrayLike:
+    """
+    Decodes a scalar, vector, or matrix plaintext `m` into a floating-point data.
+
+    Args:
+        params (PublicParameters): Cryptosystem parameters.
+        m (ArrayLike): Plaintext to be decoded.
+        delta (float): Scaling factor.
+
+    Returns:
+        ArrayLike: Decoded floating-point data.
+
+    See Also:
+        encode
+    """
+
     f = np.frompyfunc(_decode, 3, 1)
     return f(params, m, delta)
 
@@ -275,16 +540,65 @@ def decode(params: PublicParameters, m: ArrayLike, delta: float) -> ArrayLike:
 def enc(
     params: PublicParameters, pk: PublicKey, x: ArrayLike, delta: float
 ) -> int | NDArray[np.object_]:
+    """
+    Encodes and encrypts a scalar, vector, or matrix floating-point data `x` using a
+    public key `pk`.
+
+    Args:
+        params (PublicParameters): Cryptosystem parameters.
+        pk (PublicKey): Public key used for encryption.
+        x (ArrayLike): Floating-point data to be encoded and encrypted.
+        delta (float): Scaling factor.
+
+    Returns:
+        NDArray[np.object_]: Ciphertext of the encoded plaintext of the floating-point
+            data.
+
+    See Also:
+        encrypt
+        encode
+    """
+
     return encrypt(params, pk, encode(params, x, delta))
 
 
 def dec(
     params: PublicParameters, sk: SecretKey, c: int | NDArray[np.object_], delta: float
 ) -> ArrayLike:
+    """
+    Decrypts and decodes a scalar, vector, or matrix ciphertext `c` using a secret key
+    `sk`.
+
+    Args:
+        params (PublicParameters): Cryptosystem parameters.
+        sk (SecretKey): Secret key used for decryption.
+        c (NDArray[np.object_]): Ciphertext to be decrypted and decoded.
+        delta (float): Scaling factor.
+
+    Returns:
+        ArrayLike: Decoded floating-point data of the decrypted plaintext.
+
+    See Also:
+        decrypt
+        decode
+    """
+
     return decode(params, decrypt(params, sk, c), delta)
 
 
 def _encrypt(params: PublicParameters, pk: PublicKey, m: int) -> int:
+    """
+    Encrypts a message `m` using a public key `pk`.
+
+    Args:
+        params (PublicParameters): Cryptosystem parameters.
+        pk (PublicKey): Public key used for encryption.
+        m (int): Plaintext to be encrypted.
+
+    Returns:
+        int: Ciphertext of the plaintext.
+    """
+
     r = ru.get_rand(0, params.n)
     while gcd(r, params.n) != 1:
         r = ru.get_rand(0, params.n)
@@ -295,18 +609,71 @@ def _encrypt(params: PublicParameters, pk: PublicKey, m: int) -> int:
 
 
 def _decrypt(params: PublicParameters, sk: SecretKey, c: int) -> int:
+    """
+    Decrypts a ciphertext `c` using a secret key `sk`.
+
+    Args:
+        params (PublicParameters): Cryptosystem parameters.
+        sk (SecretKey): Secret key used for decryption.
+        c (int): Ciphertext to be decrypted.
+
+    Returns:
+        int: Decrypted plaintext.
+    """
+
     return (((pow(c, sk.lmd, params.n_square) - 1) // params.n) * sk.mu) % params.n
 
 
 def _add(params: PublicParameters, c1: int, c2: int) -> int:
+    """
+    Computes a ciphertext of the addition of two plaintexts corresponding to
+    ciphertexts `c1` and `c2`.
+
+    Args:
+        params (PublicParameters): Cryptosystem parameters.
+        c1 (int): Ciphertext of the first plaintext.
+        c2 (int): Ciphertext of the second plaintext.
+
+    Returns:
+        int: Ciphertext of the addition of the plaintexts.
+    """
+
     return (c1 * c2) % params.n_square
 
 
 def _int_mult(params: PublicParameters, m: int, c: int) -> int:
+    """
+    Computes a ciphertext of the product of a plaintext `m` and a plaintext
+    corresponding to a ciphertext `c`.
+
+    Args:
+        params (PublicParameters): Cryptosystem parameters.
+        m (int): Plaintext to be multiplied.
+        c (int): Ciphertext of a plaintext to be multiplied.
+
+    Returns:
+        int: Ciphertext of the product of the plaintexts.
+    """
+
     return pow(c, m, params.n_square)
 
 
 def _encode(params: PublicParameters, x: float, delta: float) -> int:
+    """
+    Encodes a floating-point number `x` into a plaintext.
+
+    Args:
+        params (PublicParameters): Cryptosystem parameters.
+        x (float): Floating-point number to be encoded.
+        delta (float): Scaling factor.
+
+    Returns:
+        int: Encoded plaintext.
+
+    Raises:
+        ValueError: If the encoded value is out of range (underflow or overflow).
+    """
+
     m = floor(x / delta + 0.5)
 
     if m < -((params.n - 1) // 2):
@@ -320,4 +687,17 @@ def _encode(params: PublicParameters, x: float, delta: float) -> int:
 
 
 def _decode(params: PublicParameters, m: int, delta: float) -> float:
+    """
+    Decodes a plaintext `m` into a floating-point number.
+
+    Args:
+        params (PublicParameters): Cryptosystem parameters.
+        m (int): Plaintext to be decoded.
+        delta (float): Scaling factor.
+
+    Returns:
+        float: Decoded floating-point number.
+
+    """
+
     return (m - floor(m / params.n + 0.5) * params.n) * delta
